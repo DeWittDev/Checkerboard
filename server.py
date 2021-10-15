ae6569d1ca5b309e78d6be29f99037e15514feb6
DeWittDev <88995967+DeWittDev@users.noreply.github.com>
2021-10-14 19:54:24 -0700

    10/14 final

--- server.py
+++ server.py
@@ -4,12 +4,16 @@ app = Flask(__name__)
 @app.route('/')
 def eights():
     num = 4
-    return render_template('index.html', column=num)
+    return render_template('index.html', num = num)
 
-@app.route('/<int:num>')
-def columnVar(num):
-    return render_template('index.html', column=num/2)
+@app.route('/<int:row>')
+def columnVar(row):
+    column = 4
+    return render_template('index.html', column = column, row = row)
 
+@app.route('/<int:row>/<int:column>')
+def sizeVar(row, column):
+    return render_template('index.html', column = column, row = row)
 
 if __name__=="__main__":
     app.run(debug=True)
